17db1f7d899dd19e4089c2bf7a86e3ee5b1d9502
ramin9819 <ramin.gh1377@gmail.com>
2021-02-15 13:03:08 +0330

    first commit

--- src/main.js
+++ src/main.js
@@ -3,4 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import BaseCard from './components/ui/BaseCard'
+// import MainBaseCard from './components/ui/MainBaseCard'
+import BaseButton from './components/ui/BaseButton'
+import BaseBadge from './components/ui/BaseBadge'
+import BaseSpinner from './components/ui/BaseSpinner'
+import BaseDialog from './components/ui/BaseDialog'
+
+const app =createApp(App)
+
+app.use(store)
+app.use(router)
+
+app.component('base-card',BaseCard)
+// app.component('main-base-card',MainBaseCard)
+app.component('base-button', BaseButton)
+app.component('base-badge', BaseBadge)
+app.component('base-spinner', BaseSpinner)
+app.component('base-dialog', BaseDialog)
+
+app.mount('#app')
---
+++ src/views/PostsList.vue
@@ -0,0 +1,10 @@
+<template>
+    <main-base-card>main ge</main-base-card>
+</template>
+
+<script>
+import MainBaseCard from '../components/ui/MainBaseCard'
+export default {
+    components:{MainBaseCard}
+}
+</script>
